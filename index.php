49743ff90f47529732c4788811e45404446ac3db
matteo-didone <matteo.dido9@gmail.com>
2023-07-05 16:33:32 +0200

    Recreated json file using php as shown in class

--- index.php
+++ index.php
@@ -1,41 +1,59 @@
-<!-- 
-Description
-We need to create a page that allows our users to use our (fairly) secure password generator. The exercise is divided into several milestones, and it is essential to develop them in an orderly manner.
-
-Milestone 1
-Create a form that sends the password length via GET. Our function will use this data to generate a random password (composed of letters, uppercase letters, numbers, and symbols) to be returned to the user. Write everything (logic and layout) in a single file, index.php.
-
-Milestone 2
-After verifying the correct functioning of our code, move the logic to a functions.php file, which we will then include on the main page.
-
-BONUS 1: Milestone 3
-Instead of displaying the password on the index page, perform a redirect to a dedicated page that will retrieve the password through $_SESSION and display it to the user.
-
-BONUS 2: Milestone 4
-Handle additional parameters for the password, such as which characters to use among numbers, letters, and symbols. They can be chosen individually (e.g., only numbers) or combined together (e.g., numbers and symbols, or all three together). Also, give the user the option to allow or disallow the repetition of identical characters.
--->
-
 <?php
-include __DIR__ . "/utilities/functions.php";
+
+$albums = [
+    [
+        "title" => "New Jersey",
+        "author" => "Bon Jovi",
+        "year" => 1988,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg",
+        "genre" => "Rock"
+    ],
+    [
+        "title" => "Live at Wembley 86",
+        "author" => "Queen",
+        "year" => 1992,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg",
+        "genre" => "Pop"
+    ],
+    [
+        "title" => "Ten\"s Summoner\"s Tales",
+        "author" => "Sting",
+        "year" => 1993,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg",
+        "genre" => "Pop"
+    ],
+    [
+        "title" => "Steve Gadd band",
+        "author" => "Steve Gadd Band",
+        "year" => 2018,
+        "poster" => "https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg",
+        "genre" => "Jazz"
+    ],
+    [
+        "title" => "Brave new World",
+        "author" => "Iron Maiden",
+        "year" => 2000,
+        "poster" => "https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg",
+        "genre" => "Metal"
+    ],
+    [
+        "title" => "One more car, one more rider",
+        "author" => "Eric Clapton",
+        "year" => 2002,
+        "poster" => "https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg",
+        "genre" => "Rock"
+    ]
+];
+
+/*
+We have to do two things:
+1) Prepare the receiver to accept a JSON file
+2) Prepare the data as a JSON object
+*/
+
+// I'm using this to send the array to the client side
+header('Content-Type: application/json');
+// Printing the array as a JSON object
+echo json_encode($albums);
+
 ?>
-
-<!DOCTYPE html>
-<html lang="it">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Strong Password Generator</title>
-    <link rel="stylesheet" href="style.css">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- JS File -->
-</head>
-
-<body>
-
-</body>
-
-</html>
