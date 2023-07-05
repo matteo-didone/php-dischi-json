374d6b2dac43b21f246619565e0749b441ffaa98
matteo-didone <matteo.dido9@gmail.com>
2023-07-05 18:27:30 +0200

    Implemented what saw in today's class

--- script/script.js
+++ script/script.js
@@ -1,10 +1,40 @@
 const { createApp } = Vue;
 
 createApp({
+
   data() {
 
     return {
-      
+      apiUrl: "./server.php",
+      items: [],
     };
   },
+
+  methods: {
+    getItems() {
+      //Axios call
+      axios.get(this.apiUrl, {
+        params: {
+
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        // Why do we need to add ".data"?
+        // Because Axios returns a response object with a data property that contains the data we requested.
+        this.items = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+    }
+  },
+
+  created() {
+    this.getItems();
+  }
+
 }).mount("#app");
---
+++ server.php
@@ -0,0 +1,14 @@
+<?php 
+    // Expose an API endpoint that returns a JSON file with the list of albums.
+    $stringRawData = file_get_contents('./database/data.json');
+
+    // Now, we need to turn the raw data that's in JSON to a format that PHP can understand, that we can work with
+    // We can do that with a function called json_decode, which is the opposite of json_encode
+    // REMEMBER:
+    // FROM JSON TO PHP --> json_decode
+    // FROM PHP TO JSON --> json_encode
+    $toDoList = json_decode($stringRawData);
+
+    header('Content-Type: application/json');
+    echo $stringRawData;
+?>
